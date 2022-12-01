c2b915dc9f9b880cf3f693c2f1b93dad012beabd
FatimaVPL <78831498+FatimaVPL@users.noreply.github.com>
2022-12-01 14:12:12 -0600

    Commit

--- app/src/main/java/com/gabyperez/notes/MainActivity.kt
+++ app/src/main/java/com/gabyperez/notes/MainActivity.kt
@@ -2,10 +2,15 @@ package com.gabyperez.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.widget.Toast
+import java.util.Locale
+import android.content.res.Configuration
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
