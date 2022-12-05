e90f1c66dec7d1db27f880eb59f01e983fa4d505
FatimaVPL <78831498+FatimaVPL@users.noreply.github.com>
2022-12-05 14:37:35 -0600

    Tabla Reminder

--- app/src/main/java/com/gabyperez/notes/data/NoteDatabase.kt
+++ app/src/main/java/com/gabyperez/notes/data/NoteDatabase.kt
@@ -6,11 +6,13 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.gabyperez.notes.model.Multimedia
 import com.gabyperez.notes.model.Note
+import com.gabyperez.notes.model.Reminder
 
-@Database(entities = [Note::class, Multimedia::class], version = 1, exportSchema = false)
+@Database(entities = [Note::class, Multimedia::class, Reminder::class], version = 1, exportSchema = false)
 abstract class NoteDatabase : RoomDatabase(){
     abstract fun NoteDao(): NoteDao
     abstract fun MultimediaDao(): MultimediaDao
+    abstract fun RerminderDAO(): RerminderDAO
 
     companion object {
         private var INSTANCE: NoteDatabase? = null
