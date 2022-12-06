0a6f72fe5e1ff01e5e7d260f3fe124add92322e5
FatimaVPL <78831498+FatimaVPL@users.noreply.github.com>
2022-12-05 21:26:32 -0600

    Abre pagina principal en notificación

--- app/src/main/java/com/gabyperez/notes/MiReceiverParaAlarma.kt
+++ app/src/main/java/com/gabyperez/notes/MiReceiverParaAlarma.kt
@@ -1,5 +1,6 @@
 package com.gabyperez.notes
 
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -10,10 +11,18 @@ val titleExtra ="TitleExtra"
 const val mensajeExtra = "messageExtra"
 
 class MiReceiverParaAlarma : BroadcastReceiver() {
-    override fun onReceive(context: Context, intent: Intent) {
+    override fun onReceive(context: Context, intent: Intent ) {
         val notificationUtils = NotificationUtils(context)
+        val activityIntent = Intent(context, MainActivity()::class.java)
+        val activityPendingIntent = PendingIntent.getActivity(
+            context,
+            1,
+            activityIntent,
+        0
+        )
         val notification = notificationUtils.getNotificationBuilder(titleExtra)
             .setContentTitle(intent.getStringExtra(titleExtra))
+            .setContentIntent(activityPendingIntent)
             .build()
         notificationUtils.getManager().notify(notificationID++, notification)
     }
